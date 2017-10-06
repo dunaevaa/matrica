82e388d0ddb4a34dd47b5c4d1bf673199f213790
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2017-10-06 11:24:06 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n, m, i, j;
+ int a[n][m];
+ int b[n][m];
+cin >> n >> m;
+ for ( i=0; i<n; i++) {
+  for ( j=0; j<m; j++)
+{
+        cin >> a[i][j];
+}
+}
+ for ( i=0; i<n; i++) { 
+  for ( j=0; j<m; j++)
+{
+        cin >> b[i][j];
+}
+}
+ for ( i=0; i<n; i++) {
+  for ( j=0; j<m; j++)
+{
+        cout << a[i][j] + b[i][j] <<" ";
+}
+}
+return 0;
+}
+
+

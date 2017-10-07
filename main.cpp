2bd49bf4356ca20fea0d63729a40922c70083a27
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2017-10-07 23:54:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main() {
  int n, m, i, j;
  int a[n][m];
  int b[n][m];
 cin >> n >> m;
+ 
  for ( i=0; i<n; i++) {
   for ( j=0; j<m; j++)
-{
-        cin >> a[i][j];
-}
-}
+    {
+      cin >> a[i][j];
+    }
+  }
+ 
  for ( i=0; i<n; i++) { 
   for ( j=0; j<m; j++)
-{
-        cin >> b[i][j];
-}
-}
+    {
+      cin >> b[i][j];
+    }
+  }
+ 
  for ( i=0; i<n; i++) {
   for ( j=0; j<m; j++)
-{
-        cout << a[i][j] + b[i][j] <<" ";
-}
-}
+    { 
+      cout << a[i][j] + b[i][j] <<" ";
+    }
+  }
 return 0;
 }
 
